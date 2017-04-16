9b68e85dbbe99da247fc0e8a9fb07d777050a3a3
amelnichnikov <alexm25890@gmail.com>
2017-04-16 21:37:46 +0300

    first

---
+++ src/main/java/com/service/Main.java
@@ -0,0 +1,11 @@
+package com.service;
+
+/**
+ * @author alexm created on 16.04.17.
+ *         testing task.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
