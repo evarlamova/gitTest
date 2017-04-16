b7f7a32a1c9e71be4d3d260f360a72ce34441703
amelnichnikov <alexm25890@gmail.com>
2017-04-16 22:00:04 +0300

    ammend
    first Third

--- src/main/java/com/service/Main.java
+++ src/main/java/com/service/Main.java
@@ -7,5 +7,11 @@ package com.service;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Second commit!");
+
+        System.out.println("Third commit!");
+
+
     }
 }
